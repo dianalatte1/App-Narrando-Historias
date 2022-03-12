8b53a479c0cb6bf4ee2b043908e0df68fe4b4708
Diana <ada_wit60@hackademy.mx>
2022-03-12 13:09:13 -0600

    Adding styles to Story Card

--- screens/StoryCard.js
+++ screens/StoryCard.js
@@ -25,7 +25,28 @@ const StoryCard = (props) => {
   } else {
     return (
       <View style={styles.container}>
-        <Text style={{ color: "white" }}>¡Tarjeta de historias!</Text>
+        <View style={styles.cardContainer}>
+          <Image
+            source={require("../assets/story_image_1.png")}
+            style={styles.storyImage}
+          ></Image>
+
+          <View style={styles.titleContainer}>
+            <Text style={styles.storyTitleText}>{this.props.story.title}</Text>
+            <Text style={styles.storyAuthorText}>
+              {this.props.story.author}
+            </Text>
+            <Text style={styles.descriptionText}>
+              {this.props.story.description}
+            </Text>
+          </View>
+          <View style={styles.actionContainer}>
+            <View style={styles.likeButton}>
+              <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+              <Text style={styles.likeText}>12m</Text>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -37,6 +58,57 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20),
+  },
+  storyImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250),
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center",
+  },
+  storyTitleText: {
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+    color: "white",
+  },
+  storyAuthorText: {
+    fontSize: RFValue(18),
+    fontFamily: "Bubblegum-Sans",
+    color: "white",
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 13,
+    color: "white",
+    paddingTop: RFValue(10),
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10),
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30),
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
   },
 });
 //No olvides exportar tu componente
