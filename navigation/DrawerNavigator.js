02ccbdac1761d2acc4ca1ccc5df0c13cdf9c9d3d
Diana Bautista <ada_wit60@hackademy.mx>
2022-03-28 12:56:17 -0600

    setting the current theme in db

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -3,7 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import BottomTabNavigator from "./TabNavigator";
 import ProfileScreen from "../screens/ProfileScreen";
 import StackNavigator from "./StackNavigator";
-
+import LogoutScreen from "../screens/LogoutScreen";
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
@@ -11,6 +11,7 @@ const DrawerNavigator = () => {
     <Drawer.Navigator>
       <Drawer.Screen name="Inicio" component={StackNavigator} />
       <Drawer.Screen name="Perfil" component={ProfileScreen} />
+      <Drawer.Screen name="Cerrar Sesión" component={LogoutScreen} />
     </Drawer.Navigator>
   );
 };
